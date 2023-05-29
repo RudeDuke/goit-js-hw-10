e7ada2910867fec91915dcc5cb7d569d4d21c7ba
Ruduke <dima.rudiuk@gmail.com>
2023-05-29 16:34:03 +0200

    Optimized functions in cat-api.js
     - Converted to arrow functions;
    - Converted to asynchronous functions

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,41 +1,42 @@
-const API_KEY = 'live_DvnJdN84k9FRjVQJ7l8jidEauONyZVmQE2TyRcJyoewxBgH0kpbHcM0SS4QPcx5Q';
+const API_KEY =
+  'live_DvnJdN84k9FRjVQJ7l8jidEauONyZVmQE2TyRcJyoewxBgH0kpbHcM0SS4QPcx5Q';
 
-export function fetchBreeds() {
+export const fetchBreeds = async () => {
   const url = 'https://api.thecatapi.com/v1/breeds';
 
-  return fetch(url, {
-    headers: {
-      'x-api-key': API_KEY
-    }
-  })
-    .then(response => response.json())
-    .then(data => data.map(breed => ({ id: breed.id, name: breed.name })))
-    .catch(error => {
-      console.error('Error fetching breeds:', error);
-      throw error;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'x-api-key': API_KEY,
+      },
     });
-}
+    const data = await response.json();
+    return data.map(breed => ({ id: breed.id, name: breed.name }));
+  } catch (error) {
+    console.error('Error fetching breeds:', error);
+    throw error;
+  }
+};
 
-export function fetchCatByBreed(breedId) {
+export const fetchCatByBreed = async breedId => {
   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
 
-  return fetch(url, {
-    headers: {
-      'x-api-key': API_KEY
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      const cat = data[0];
-      return {
-        imageUrl: cat.url,
-        breedName: cat.breeds[0].name,
-        description: cat.breeds[0].description,
-        temperament: cat.breeds[0].temperament
-      };
-    })
-    .catch(error => {
-      console.error('Error fetching cat by breed:', error);
-      throw error;
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'x-api-key': API_KEY,
+      },
     });
-}
+    const data = await response.json();
+    const cat = data[0];
+    return {
+      imageUrl: cat.url,
+      breedName: cat.breeds[0].name,
+      description: cat.breeds[0].description,
+      temperament: cat.breeds[0].temperament,
+    };
+  } catch (error) {
+    console.error('Error fetching cat by breed:', error);
+    throw error;
+  }
+};
